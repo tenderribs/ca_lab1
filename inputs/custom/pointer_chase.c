1649447cee1c43347049766bcb8213ba221a42ee
mark <mark@Mark-MBP>
2025-10-10 16:24:11 +0200

    seems legit

--- inputs/custom/pointer_chase.c
+++ inputs/custom/pointer_chase.c
@@ -1,38 +1,51 @@
+#include "stdio.h"
 #include <stdint.h>
 
-typedef struct Node {
-    uint32_t value;
-    struct Node *next;
-} Node;
-
-// Static allocation in global space
-#define NUM_NODES 16384
+#define NUM_NODES (32 * 1024)
 #define CHASE_COUNT 100000
-static Node nodes[NUM_NODES];
 
 int main() {
+    uint16_t nodes[NUM_NODES];
+
     // Initialize PRNG with same seed as assembly
-    uint32_t random_seed = 7919;
+    uint32_t random_seed = 1337;
+    const uint32_t A = 48271;
+
+    // // double check size of nodes
+    // printf("sizeof(nodes): %lu", sizeof(nodes));
 
     // Initialize nodes with values and random next pointers
     for (int i = 0; i < NUM_NODES; i++) {
-        nodes[i].value = i;
 
         // Generate random next pointer using MINSTD LCG algorithm
-        // https://en.wikipedia.org/wiki/Lehmer_random_number_generator
-        random_seed = (random_seed * 16807) & 0x7FFFFFFF;
+        // https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
+        // https://en.wikipedia.org/wiki/Lehmer_random_number_generator#Sample_C99_code
+
+        // to avoid 64 bit arithmetic, use legit same code as example in
+        // wikipedia max: 32,767 * 48,271 = 1,581,695,857 = 0x5e46c371
+        uint32_t low = (random_seed & 0x7fff) * A;
+        // max: 65,535 * 48,271 = 3,163,439,985 = 0xbc8e4371
+        uint32_t high = (random_seed >> 15) * A;
+        // max: 0x5e46c371 + 0x7fff8000 + 0xbc8e = 0xde46ffff
+        uint32_t x = low + ((high & 0xffff) << 15) + (high >> 16);
+
+        random_seed = (x & 0x7fffffff) + (x >> 31);
+
         int random_index = random_seed % NUM_NODES;
 
-        nodes[i].next = &nodes[random_index];
+        nodes[i] = random_index;
+
+        // // to check distribution for actual randomness
+        // printf("%d, %d\r\n", i + 1, random_index);
     }
 
     // Pointer-chasing loop
-    Node *current = &nodes[0];
     volatile uint32_t sum = 0; // volatile so -03 gcc flag doesn't optimize away
 
+    uint32_t curr = nodes[0];
     for (int i = 0; i < CHASE_COUNT; i++) {
-        sum += current->value;
-        current = current->next;
+        sum += curr;
+        curr = nodes[curr]; // go to next
     }
 
     return 0;
--- inputs/custom/pointer_chase.s
+++ inputs/custom/pointer_chase.s
@@ -1,49 +1,43 @@
-# compiled with https://godbolt.org, https://stackoverflow.com/a/63386888
-# Step 1 compile C to ASM: mips gcc 15.2.0 with flags -xc -O3 -march=mips32 -Wall -fverbose-asm -fno-delayed-branch
-# Step 2 modify assembly: 1: add .text with j main, 2. replace j $31 with syscall
-# Step 3: Assemble to Hex machine code using MARS simulator
-
-.text
-	j main
 main:
-        li      $3,131072             # 0x20000        #,
+        li      $3,524288             # 0x80000        #,
         ori     $3,$3,0x10         #,,
         subu    $sp,$sp,$3       #,,
-        addiu   $4,$sp,8       # tmp229,,
-        li      $7,2147418112                 # 0x7fff0000     # tmp212,
-        move    $6,$4    # ivtmp.18, tmp229
+        addiu   $4,$sp,8       # tmp261,,
+        li      $7,2147418112                 # 0x7fff0000     # tmp265,
+        move    $6,$4    # ivtmp.17, tmp261
         move    $5,$0    # i,
         li      $3,7919           # 0x1eef         # random_seed,
-        li      $9,16807                    # 0x41a7         # tmp210,
-        ori     $7,$7,0xffff       # tmp211, tmp212,
-        li      $8,16384                    # 0x4000         # tmp216,
+        li      $9,48271                    # 0xbc8f         # tmp264,
+        ori     $7,$7,0xffff       # tmp266, tmp265,
+        li      $8,65536                    # 0x10000        # tmp267,
 $L2:
-        mul     $3,$3,$9   # _2, random_seed, tmp210
-        sw      $5,0($6)     # i, MEM[(unsigned int *)_10]
-        addiu   $6,$6,8        # ivtmp.18, ivtmp.18,
+        mul     $3,$3,$9   # D.1613, random_seed, tmp264
+        sw      $5,0($6)     # i, MEM[base: _6, offset: 0B]
+        andi    $2,$3,0xffff     # random_index, D.1613,
+        sll     $2,$2,3    # tmp246, random_index,
+        addu    $2,$4,$2         # D.1615, tmp261, tmp246
         addiu   $5,$5,1        # i, i,
-        andi    $2,$3,0x3fff     # _15, _2,
-        sll     $2,$2,3    # _36, _15,
-        addu    $2,$4,$2         # _3, tmp229, _36
-        sw      $2,-4($6)    # _3, MEM[(struct Node * *)_10 + 4B]
-        and     $3,$3,$7   # random_seed, _2, tmp211
+        sw      $2,4($6)     # D.1615, MEM[base: _6, offset: 4B]
+        and     $3,$3,$7   # random_seed, D.1613, tmp266
+        addiu   $6,$6,8        # ivtmp.17, ivtmp.17,
         bne     $5,$8,$L2
         nop
-        li      $5,131072             # 0x20000        # tmp217,
-        addu    $6,$4,$5         # tmp218, tmp229, tmp217
-        move    $2,$4    # current, tmp229
-        addiu   $5,$5,-31072   # ivtmp_28, tmp217,
-        sw      $0,0($6)     #, sum
+        li      $3,524288             # 0x80000        # tmp249,
+        addu    $3,$4,$3         # tmp250, tmp261, tmp249
+        move    $2,$4    # current, tmp261
+        sw      $0,0($3)     #, sum
+        li      $5,10000                    # 0x2710         # D.1616,
+        move    $4,$3    # tmp263, tmp250
 $L3:
-        lw      $3,0($6)             # sum.2_5, sum
-        lw      $4,0($2)             # current_33->value, current_33->value
-        addiu   $5,$5,-1       # ivtmp_28, ivtmp_28,
-        addu    $3,$3,$4         # _6, sum.2_5, current_33->value
-        lw      $2,4($2)             # current, current_33->next
-        sw      $3,0($6)     # _6, sum
+        lw      $3,0($4)     # D.1613, sum
+        lw      $6,0($2)     # current_33->value, current_33->value
+        addiu   $5,$5,-1       # D.1616, D.1616,
+        addu    $3,$3,$6         # D.1613, D.1613, current_33->value
+        sw      $3,0($4)     # D.1613, sum
+        lw      $2,4($2)     # current, current_33->next
         bne     $5,$0,$L3
         nop
-        li      $8,131072             # 0x20000        #,
+        li      $8,524288             # 0x80000        #,
         ori     $8,$8,0x10         #,,
         move    $2,$0    #,
         addu    $sp,$sp,$8       #,,
